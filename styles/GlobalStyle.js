dd86895ab254bfc49846e87415f081dd933744fd
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-05 21:02:57 -0300

    feat: Criação da estilização Global e tema

---
+++ styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle} from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        width: 100%;
+        min-height: 100vh;
+    }
+
+
+
+`
---
+++ styles/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+    dark:'#1c2229',
+    gray:'#dad9dd',
+    brown:'#debea0',
+    pastel:'#e3a463',
+    orange:'#e88a27'
+}
