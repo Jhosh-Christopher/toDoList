47e74606e7acd1e70fde37bbe511630612ea8385
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-11 19:14:18 -0300

    refactor: Remoção de importação

--- src/components/BarraSearch/BarraSearch.jsx
+++ src/components/BarraSearch/BarraSearch.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import {BarraStyles} from './barra.styles.js'
-import  {SetBtn} from '../../assets/SetBtn.jsx'
 import { theme } from '../../../styles/theme.js'
 const BarraSearch = () => {
   return (
     <BarraStyles>
         <input className={'input-box'} type="text" placeholder="Digite Aqui" />
         <button className={'button'}>ALL</button>
-        <button className={'BtnSvg'}><SetBtn fill={theme.gray}></SetBtn></button>
     </BarraStyles>
   )
 }
