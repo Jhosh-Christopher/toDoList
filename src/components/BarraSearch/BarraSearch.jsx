394ef6e7fe7e3a934e302972624cb1cb10d069dc
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-10 21:15:43 -0300

    feat: Importação do botão jsx alterando a cor do mesmo

--- src/components/BarraSearch/BarraSearch.jsx
+++ src/components/BarraSearch/BarraSearch.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BarraStyles} from './barra.styles.js'
+import  {SetBtn} from '../../assets/SetBtn.jsx'
+import { theme } from '../../../styles/theme.js'
 const BarraSearch = () => {
   return (
     <BarraStyles>
-    <div>
         <input className={'input-box'} type="text" placeholder="Digite Aqui" />
-    </div>
+        <button className={'button'}>ALL</button>
+        <button className={'BtnSvg'}><SetBtn fill={theme.gray}></SetBtn></button>
     </BarraStyles>
   )
 }
--- src/components/BarraSearch/barra.styles.js
+++ src/components/BarraSearch/barra.styles.js
@@ -4,6 +4,8 @@ export const BarraStyles = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 100%;
+    gap: 10px;
 
     .input-box{
         width: 420px;
@@ -15,6 +17,23 @@ export const BarraStyles = styled.div`
         height: 38px;
         padding: 8px 16px;
         align-items: center;
-        gap: 436px;
+    }
+
+    .button{
+        display: flex;
+        height: fit-content;
+        width: fit-content;
+        align-items: center;
+        justify-content: center;
+        /* padding: 10px; */
+        color: ${(props) => props.theme.brown};
+        background-color: ${(props) => props.theme.orange};
+    }
+
+    .BtnSvg{
+        width: 30px;
+        height: 30px;
+        flex-shrink: 0;
+        border-radius: 15px;
     }
     `
