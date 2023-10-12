8b977c1139d312725c7938a314db25f7b5e371f8
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-11 23:37:10 -0300

    feat: Implementação da função de adicionar task, criando uma li

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { ListStyles } from './list.styles'
-const List = () => {
+import { Item, ListStyles } from './list.styles'
+const List = ({todoList}) => {
+
+  const Lista = todoList.map(task =>{
+    
+  })
+
   return (
     <ListStyles>
         <div>
           <ul>
-              <p>TERMINAR O PROJETO</p>
+            {todoList.map((task)=>
+            <Item>
+              <p>{task}</p>
               <section>
                 <button><i class='bx bx-check-circle'></i></button>
                 <button><i class='bx bxs-trash'></i></button>
               </section>
+              </Item>
+              )}
           </ul>
         </div>
     </ListStyles>
