c31b5ac7a14bd1a6544b0f492e0ef28288bccd8b
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-11 21:33:42 -0300

    feat: Inserção do plus icon no botão

--- src/components/BarraSearch/BarraSearch.jsx
+++ src/components/BarraSearch/BarraSearch.jsx
@@ -5,7 +5,7 @@ const BarraSearch = () => {
   return (
     <BarraStyles>
         <input className={'input-box'} type="text" placeholder="Digite Aqui" />
-        <button className={'button'}>ALL</button>
+        <button className={'button'}><i class='bx bx-plus-circle'></i></button>
     </BarraStyles>
   )
 }
