8ffc0e8706f6c5b03d2c5d0d858d1b6c100ee53b
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-13 11:30:00 -0300

    feat: Implementando as funções de checar, excluir e editar(Cancelar e Salvar) a task

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,28 +1,89 @@
-import React from 'react'
-import { Item, ListStyles } from './list.styles'
-const List = ({todoList}) => {
+import React from "react";
+import { Item, ListStyles } from "./list.styles";
+import { useState } from "react";
+const List = ({ todoList, setTodoList }) => {
+  const [editedTask, setEditedTask] = useState(null);
+  const [editedText, setEditedText] = useState("");
+  const removeTask = (id) => {
+    const updatedList = todoList.filter((task) => task.id !== id);
+    setTodoList(updatedList);
+  };
 
-  const Lista = todoList.map(task =>{
-    
-  })
+  const toggleCheck = (id, checked) => {
+    const updatedList = todoList.map((task) => {
+      if (task.id === id) {
+        return { ...task, checked: !checked };
+      }
+      return task;
+    });
+    setTodoList(updatedList);
+  };
+
+  const editTask = (id, task) => {
+    setEditedTask(id);
+    setEditedText(task);
+  };
+
+  const saveEdit = (id) => {
+    const updatedList = todoList.map((task) => {
+      if (task.id === id) {
+        return { ...task, task: editedText };
+      }
+      return task;
+    });
+    setTodoList(updatedList);
+    setEditedTask(null);
+  };
 
   return (
     <ListStyles>
-        <div>
-          <ul>
-            {todoList.map((task)=>
-            <Item>
-              <p>{task}</p>
-              <section>
-                <button><i class='bx bx-check-circle'></i></button>
-                <button><i class='bx bxs-trash'></i></button>
-              </section>
+      <div>
+        <ul>
+          {todoList.map((task) => (
+            <>
+              <Item checked={task.checked} key={task.id}>
+                {editedTask === task.id ? (
+                  <input
+                    type="text"
+                    value={editedText}
+                    onChange={(e) => setEditedText(e.target.value)}
+                  />
+                ) : (
+                  <p>{task.task}</p>
+                )}
+                <section>
+                  {editedTask === task.id ? (
+                    <>
+                      <button onClick={() => saveEdit(task.id)}>
+                        <i class="bx bx-save"></i>
+                      </button>
+                      <button onClick={() => setEditedTask(null)}>
+                        <i class="bx bx-x-circle"></i>
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => toggleCheck(task.id, task.checked)}
+                      >
+                        <i className="bx bx-check-circle"></i>
+                      </button>
+                      <button onClick={() => editTask(task.id, task.task)}>
+                        <i className="bx bxs-edit"></i>
+                      </button>
+                      <button onClick={() => removeTask(task.id)}>
+                        <i className="bx bxs-trash"></i>
+                      </button>
+                    </>
+                  )}
+                </section>
               </Item>
-              )}
-          </ul>
-        </div>
+            </>
+          ))}
+        </ul>
+      </div>
     </ListStyles>
-  )
-}
+  );
+};
 
-export default List
+export default List;
