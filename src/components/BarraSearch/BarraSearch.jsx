7225de9cfe9a1efa9478477356e5bca44f61d5fb
Jhosh-Christopher <shuanbrj@gmail.com>
2023-10-13 11:26:02 -0300

    feat: finalização da função de click adicionando a lista e setando a task

--- src/components/BarraSearch/BarraSearch.jsx
+++ src/components/BarraSearch/BarraSearch.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
-import {BarraStyles} from './barra.styles.js'
+import React, { useState } from "react";
+import { BarraStyles } from "./barra.styles.js";
 
-const BarraSearch = ({addList}) => {
+const BarraSearch = ({ addList }) => {
+  const [todo, setTodo] = useState("");
 
-  const  [todo, setTodo] = useState('')
-
-  function clicou(){
-    addList(todo)
+  function clicou() {
+    addList(todo);
+    setTodo("");
   }
-  
 
   return (
     <BarraStyles>
-        <input className={'input-box'} type="text" value={todo} placeholder="Digite Aqui" 
-        onChange={(e) => setTodo(e.target.value)}/>
-        <button className={'button'}  onClick={clicou}><i class='bx bx-plus-circle'></i></button>
+      <input
+        className={"input-box"}
+        type="text"
+        value={todo}
+        placeholder="Digite Aqui sua Tarefa"
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className={"button"} onClick={clicou}>
+        <i class="bx bx-plus-circle"></i>
+      </button>
     </BarraStyles>
-  )
-}
+  );
+};
 
-export default BarraSearch
+export default BarraSearch;
